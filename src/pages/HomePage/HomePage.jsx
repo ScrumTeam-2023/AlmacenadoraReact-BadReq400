a294d6aec6be8e001c3ed9915154cde8df56ce53
M <malvarez-2018477@kinal.edu.gt>
2023-05-03 19:29:06 -0600

    Se agrego Login (TEMP)
    -Login funcional (Sin estilo)

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import React from 'react'
 import './HomeStyle.css'
-import image from "../HomePage/BGimage/TestBGMain.png"
 
 // [CODIGO]
 export const HomePage = () => {
@@ -13,7 +12,7 @@ export const HomePage = () => {
 // [HTML]
   return (
    <>
-   <h1>Hello</h1>
+   <h1>HomePage</h1>
    
    </>
   )
---
+++ src/pages/UserPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const UsserPage = () => {
+    return (
+    <h1>User Page </h1>
+    )
+}
